214a9d4e155824e8a0857e442f556383a1790047
Henry Bernreuter <42005962+HenryBernreuter@users.noreply.github.com>
2018-09-09 09:50:51 -0400

    Add files via upload

--- Chapter2Assignment.java
+++ Chapter2Assignment.java
@@ -1,5 +1,6 @@
 package chapter2;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Chapter2Assignment {
 	public static void main(String[] args) {
@@ -10,20 +11,20 @@ public class Chapter2Assignment {
 			
 		// Create input of Most recent per share
 		System.out.print("Enter Most recent per share dividend: ");
-		float MostRecentPerShareDividend = input.nextFloat();
+		double MostRecentPerShareDividend = input.nextDouble();
 		System.out.println(MostRecentPerShareDividend);
 		// Create input discount rate
 		System.out.print("Enter Required return or Disount rate:");
-		float DiscountRate = input.nextFloat()/100;
+		double DiscountRate = input.nextDouble()/100;
 		System.out.println(DiscountRate);
 		// Create  input rate of growth
 		System.out.print("Enter Rate of Growth:");
-		float RateOfGrowth = input.nextFloat()/100;
+		double RateOfGrowth = input.nextDouble()/100;
 		System.out.println(RateOfGrowth);
-		
+		DecimalFormat df = new DecimalFormat("###.##");
+		double value = MostRecentPerShareDividend*(1 + RateOfGrowth)*(DiscountRate-RateOfGrowth)*100;
 		//  export value of one share of common stock
-		System.out.print("The value of one share of common stock is:" +
-				MostRecentPerShareDividend*(1 + RateOfGrowth)*(DiscountRate-RateOfGrowth));
+		System.out.print("The value of one share of common stock is:" + "$"+ df.format(value));
 	}
 
 }
